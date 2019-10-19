fd26046696c4413903a9238e6b753b7abb549841
Deniz Toprak <contact.deniz.toprak@gmail.com>
2019-10-19 12:56:31 +0200

    initial commit with solution settings, test main

---
+++ cpp_klink_elective/SFMLEngine/Main.cpp
@@ -0,0 +1,46 @@
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+	int i = 0;
+	while (window.isOpen())
+	{
+		++i;
+		switch (i)
+		{
+		case 0:
+			shape.setFillColor(sf::Color::Green);
+			break;
+		case 1:
+			shape.setFillColor(sf::Color::Red);
+			break;
+		case 2:
+			shape.setFillColor(sf::Color::Yellow);
+			break;
+		case 3:
+			shape.setFillColor(sf::Color::Blue);
+			break;
+		case 4:
+			shape.setFillColor(sf::Color::White);
+			i = 0;
+			break;
+		}
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
