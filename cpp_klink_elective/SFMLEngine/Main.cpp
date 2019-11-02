97c0a2e9027724e9e8bcce574a42a137a5fd62f3
Deniz Toprak <deniz.toprak@stud.h-da.de>
2019-11-02 11:23:39 +0100

    previous commit is faulty

--- cpp_klink_elective/SFMLEngine/Main.cpp
+++ cpp_klink_elective/SFMLEngine/Main.cpp
@@ -1,23 +1,88 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <array>
+#include <list>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	std::vector<sf::Drawable*> drawables;
+
+	const float moveSpeed = 50.f;
+	sf::RenderWindow stackWindow(sf::VideoMode(800, 600), "SFML works!");
+
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+	shape.setOrigin(sf::Vector2f(shape.getRadius(), shape.getRadius()));
+	shape.setPosition(sf::Vector2f(stackWindow.getSize().x / 2.0f, stackWindow.getSize().y / 2.0f));
 
-	while (window.isOpen())
+	sf::RectangleShape shape2(sf::Vector2f(50, 25));
+	shape2.setFillColor(sf::Color::Yellow);
+
+	sf::Sprite mySprite;
+	sf::Texture myTexture;
+	if (myTexture.loadFromFile("./FE_Lyn.png"))
 	{
+		mySprite.setTexture(myTexture);
+	}
+
+	sf::Clock clock;
+	sf::Time lastFrameTime;
+
+	drawables.push_back(&shape);
+	drawables.push_back(&shape2);
+	drawables.push_back(&mySprite);
+
+	while (stackWindow.isOpen())
+	{
+		//Game Loop
+		sf::Time currentTime = clock.getElapsedTime();
+		float deltaSeconds = (currentTime - lastFrameTime).asMicroseconds() / 1000.0f / 1000.0f;
+
+		std::cout << deltaSeconds << "\n";
+
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (stackWindow.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();
+			{
+				stackWindow.close();
+			}
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+		sf::Vector2f moveVector;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			moveVector.y -= moveSpeed * deltaSeconds;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			moveVector.x -= moveSpeed * deltaSeconds;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			moveVector.y += moveSpeed * deltaSeconds;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			moveVector.x += moveSpeed * deltaSeconds;
+		}
+
+		std::vector<int> myVec; //List in C#
+		std::list<int> myList; //Linked List in C#
+
+		mySprite.move(moveVector);
+
+		stackWindow.clear();
+
+		//Display all drawables
+		for (int i = 0; i < drawables.size(); i++)
+		{
+			stackWindow.draw(*drawables[i]);
+		}
+
+		stackWindow.display();
+
+		lastFrameTime = currentTime;
 	}
 
 	return 0;
