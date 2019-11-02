7e0bbf2dec9f5dbdc59aca4fe1f16521ee65c110
Deniz Toprak <deniz.toprak@stud.h-da.de>
2019-11-02 11:19:06 +0100

    Changes from 26th Oct, wiki entry following shortly

--- cpp_klink_elective/SFMLEngine/Main.cpp
+++ cpp_klink_elective/SFMLEngine/Main.cpp
@@ -1,36 +1,13 @@
 #include <SFML/Graphics.hpp>
 
-
 int main()
 {
-	//Test
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
-	int i = 0;
+
 	while (window.isOpen())
 	{
-		++i;
-		switch (i)
-		{
-		case 0:
-			shape.setFillColor(sf::Color::Green);
-			break;
-		case 1:
-			shape.setFillColor(sf::Color::Red);
-			break;
-		case 2:
-			shape.setFillColor(sf::Color::Yellow);
-			break;
-		case 3:
-			shape.setFillColor(sf::Color::Blue);
-			break;
-		case 4:
-			shape.setFillColor(sf::Color::White);
-			i = 0;
-			break;
-		}
-
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
