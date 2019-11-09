b4fb1e5c7c04611f3ca96ffdf87d2ec032bb10fa
Deniz Toprak <deniz.toprak@stud.h-da.de>
2019-11-09 10:23:00 +0100

    EntityID from previous session

--- cpp_klink_elective/SFMLEngine/Main.cpp
+++ cpp_klink_elective/SFMLEngine/Main.cpp
@@ -3,9 +3,99 @@
 #include <array>
 #include <list>
 
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
+
+struct EntityId
+{
+	EntityId()
+		: index(0)
+		, salt(0)
+	{ }
+
+	EntityId(sf::Uint16 _index, sf::Uint16 _salt)
+		: index(_index)
+		, salt(_salt)
+	{ }
+
+	sf::Uint16 index;
+	sf::Uint16 salt;
+
+	bool operator==(EntityId& rhs) const
+	{
+		return index == rhs.index && salt == rhs.salt;
+	}
+};
+
+EntityId s_invalidEntityId = EntityId();
+
+class Entity
+{
+public:
+
+	EntityId myId;
+	std::string name;
+
+	sf::Drawable* pDrawable = nullptr;
+
+	bool IsAlive() const
+	{
+		return myId.index != s_invalidEntityId.index;
+	}
+};
+
+std::array<Entity, std::numeric_limits<sf::Uint16>::max()> entities;
+
+
+EntityId AddEntity()
+{
+	for (int i = 1; i < entities.size(); i++)
+	{
+		const bool isAlive = entities[i].IsAlive();
+		if (!isAlive)
+		{
+			EntityId newId(i, entities[i].myId.salt + 1);
+			entities[i].myId = newId;
+			return newId;
+		}
+	}
+
+	return EntityId();
+}
+
+Entity* GetEntityByID(EntityId id)
+{
+	if (id.index == 0 || id.salt == 0)
+	{
+		return nullptr;
+	}
+
+	if (entities[id.index].IsAlive() && entities[id.index].myId.salt == id.salt)
+	{
+		//salt values match, reffering to same entity
+		return &entities[id.index];
+	}
+	return nullptr;
+}
+
+bool DestroyEntity(EntityId id)
+{
+	Entity* pEntity = GetEntityByID(id);
+	if (pEntity != nullptr)
+	{
+		pEntity->myId.index = 0;
+		return false;
+	}
+
+	return false;
+}
+
 int main()
 {
-	std::vector<sf::Drawable*> drawables;
+	//random generator
+	srand(time(0));
+
+	std::vector<EntityId> entityIds;
 
 	const float moveSpeed = 50.f;
 	sf::RenderWindow stackWindow(sf::VideoMode(800, 600), "SFML works!");
@@ -15,9 +105,18 @@ int main()
 	shape.setOrigin(sf::Vector2f(shape.getRadius(), shape.getRadius()));
 	shape.setPosition(sf::Vector2f(stackWindow.getSize().x / 2.0f, stackWindow.getSize().y / 2.0f));
 
+	//Create circle shapes
+	entityIds.push_back(AddEntity());
+	GetEntityByID(entityIds.back())->pDrawable = &shape;
+
+	//Create rectangle shapes
 	sf::RectangleShape shape2(sf::Vector2f(50, 25));
 	shape2.setFillColor(sf::Color::Yellow);
 
+	entityIds.push_back(AddEntity());
+	GetEntityByID(entityIds.back())->pDrawable = &shape2;
+
+	//Create sprite shapes
 	sf::Sprite mySprite;
 	sf::Texture myTexture;
 	if (myTexture.loadFromFile("./FE_Lyn.png"))
@@ -25,12 +124,25 @@ int main()
 		mySprite.setTexture(myTexture);
 	}
 
+	entityIds.push_back(AddEntity());
+	GetEntityByID(entityIds.back())->pDrawable = &mySprite;
+
+	//Create random circle
+	sf::CircleShape cshape((rand() % 100) + 1);
+	cshape.setFillColor(sf::Color::Green);
+	cshape.setOrigin(sf::Vector2f(cshape.getRadius(), cshape.getRadius()));
+	cshape.setPosition(
+		sf::Vector2f(
+		(rand() % stackWindow.getSize().x),
+			(rand() % stackWindow.getSize().y)
+		)
+	);
+
+	entityIds.push_back(AddEntity());
+	GetEntityByID(entityIds.back())->pDrawable = &cshape;
+
 	sf::Clock clock;
 	sf::Time lastFrameTime;
-
-	drawables.push_back(&shape);
-	drawables.push_back(&shape2);
-	drawables.push_back(&mySprite);
 
 	while (stackWindow.isOpen())
 	{
@@ -72,12 +184,40 @@ int main()
 
 		mySprite.move(moveVector);
 
+
+		DestroyEntity(entityIds.back());
+
+		sf::CircleShape cshape((rand() % 100) + 1);
+		cshape.setFillColor(sf::Color::Green);
+		cshape.setOrigin(sf::Vector2f(cshape.getRadius(), cshape.getRadius()));
+		cshape.setPosition(
+			sf::Vector2f(
+			(rand() % stackWindow.getSize().x),
+				(rand() % stackWindow.getSize().y)
+			)
+		);
+
+		entityIds.push_back(AddEntity());
+		GetEntityByID(entityIds.back())->pDrawable = &cshape;
+
+		
+
+		
+
+
+
+
 		stackWindow.clear();
 
 		//Display all drawables
-		for (int i = 0; i < drawables.size(); i++)
+		for (int i = 0; i < entityIds.size(); i++)
 		{
-			stackWindow.draw(*drawables[i]);
+			Entity* pEntity = GetEntityByID(entityIds[i]);
+			if (pEntity)
+			{
+				stackWindow.draw(*pEntity->pDrawable);
+
+			}
 		}
 
 		stackWindow.display();
